eaa46ef8ee0ebfed38b09428dabb8ea130079b31
Juan Reyes <juanmanuelreyesarrambide@gmail.com>
2021-08-07 19:24:53 -0300

    2.0

---
+++ js/script.js
@@ -0,0 +1,50 @@
+// Smooth
+(function(){
+
+    var parallax = document.querySelectorAll("body"),
+        speed = .2;
+
+    window.onscroll = function(){
+      [].slice.call(parallax).forEach(function(el,i){
+
+        var windowYOffset = window.pageYOffset,
+            elBackgrounPos = "50% " + (windowYOffset * speed) + "px";
+
+        el.style.backgroundPosition = elBackgrounPos;
+      });
+    };
+  })();
+
+
+
+// Carrusel
+$(".owl-carousel").owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplaySpeed:3000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+// Responsive Button
+
+$(".menu-btn").click(function(){
+    $(this).toggleClass("fa-times");
+    $(".headerNav-ul").toggleClass("active");
+  });
+
+$(".boton-off").click(function(){
+    $(this).toggleClass("boton");
+});
